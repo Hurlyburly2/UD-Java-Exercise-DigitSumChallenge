5758ba6e45a109cf459d8bf338e802a1b7f64004
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-22 21:56:18 -0400

    initial commit

---
+++ src/com/dsimon/Main.java
@@ -0,0 +1,8 @@
+package com.dsimon;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(SumDigits.sumDigits(125) == 8);
+    }
+}
---
+++ src/com/dsimon/SumDigits.java
@@ -0,0 +1,7 @@
+package com.dsimon;
+
+public class SumDigits {
+    public static int sumDigits(int number) {
+
+    }
+}
